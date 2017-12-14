82fcaafb9710798032efeea17073d9ce00adfc9c
Ashley Jordan <ajordanium@gmail.com>
2017-12-14 10:43:09 -0800

    answered comment four in server.js

--- greg-ashley-ed/server.js
+++ greg-ashley-ed/server.js
@@ -73,7 +73,9 @@ app.post('/articles', (request, response) => {
 
 app.put('/articles/:id', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // Number 3 of the diagram corresponds with this line of code. 
+  // Interacting with: Article.prototype.updateRecord
+  // CRUD: UPDATE.
   client.query(
     `UPDATE articles
     SET
