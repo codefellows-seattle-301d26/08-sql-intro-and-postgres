7ddd893234a50d0552dcca3387e656bbb1c40917
Ashley Jordan <ajordanium@gmail.com>
2017-12-14 12:45:36 -0800

    remove the unneeded json code

--- greg-ashley-ed/public/scripts/articleView.js
+++ greg-ashley-ed/public/scripts/articleView.js
@@ -103,8 +103,8 @@ articleView.create = () => {
     hljs.highlightBlock(block);
   });
 
-  $('#export-field').show();
-  $('#article-json').val(`${JSON.stringify(article)},`);
+  // $('#export-field').show();
+  // $('#article-json').val(`${JSON.stringify(article)},`);
 };
 
 articleView.submit = event => {
