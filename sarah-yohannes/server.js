7b22b373dbaeea6658efeca8b32e77169bd705d7
Sarah Bixler <bixler.s.e@gmail.com>
2017-12-14 10:12:10 -0800

    also added database url in server.js and passed to client as argument

--- sarah-yohannes/server.js
+++ sarah-yohannes/server.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const express = require('express');
 const { Client } = require('pg');
-
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
